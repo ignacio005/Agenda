cd653abef84e4d9ec482146a976901ec08e03114
ignacio005 <ignacio.tiradomeza@usp.ceu.es>
2023-11-03 23:55:34 +0100

    Nuevos cambios

--- src/dominio/Libreta.java
+++ src/dominio/Libreta.java
@@ -5,9 +5,20 @@ public class Libreta {
 	
 	private ArrayList <Contacto> lista= new ArrayList <Contacto> ();
 	
+	public ArrayList <Contacto> getLista() {
+		
+		return lista;
+	}
+	
 	public void addContacto (Contacto c) {
 		
 		lista.add(c);
+	}
+	
+	public void removeContacto (int posicion) {
+		
+		lista.remove(posicion);
+		
 	}
 	
 	@Override
--- src/interfaz/Interfaz.java
+++ src/interfaz/Interfaz.java
@@ -25,12 +25,17 @@ public class Interfaz {
 		
 		else if (p.length==1) {
 			
+			if (p[0].equals("Agregar contacto")) {
+				
+				addContacto(p[1], l);
+			}
+			
 			if (p[0].equals("Ayuda")) {
 				
 				System.out.println("Introduzca las siguientes peticiones: "
 				+ "\n Agregar contacto: (escribir nombre y numero de teléfono)"
 				+ "\n Lista: (listar el contenido) "
-				+ "\n Leer: (lectura inicial) "
+				+ "\n Eliminar contacto: (elimina contacto)"
 				+ "\n Salir: (escribe salir)");
 			}
 			
@@ -38,6 +43,11 @@ public class Interfaz {
 				
 				System.out.println(l);
 				
+			}
+			
+			else if (p[0].equals("Eliminar contacto")) {
+				
+				removeContacto(l);
 			}
 			
 			else if (p[0].equals("Salir")) {
@@ -56,6 +66,7 @@ public class Interfaz {
 			
 				addContacto(p[1], l);
 			}
+			
 
 		return true;
 	}
@@ -68,7 +79,21 @@ public class Interfaz {
 		Contacto contacto = new Contacto (nombre,numeroDetelefono);
 		l.addContacto(contacto);
 		
+	}
+	
+	public static void removeContacto(Libreta l) {
+		
+		ArrayList<Contacto> contactos= l.getLista();
+		for(int i=0; i<l.getLista().size(); i++) {
 			
+				System.out.println(i+":"+ contactos.get(i).getNombre() + " " + contactos.get(i).getNumeroDetelefono() );
+			
+		}
+		
+		System.out.println("Introduzca posición del contacto: ");
+		int i = Integer.parseInt(sc.nextLine());
+		l.removeContacto(i);
+		
 	}
 	
 	public static String leerPeticion() {
